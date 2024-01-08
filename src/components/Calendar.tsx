0f588f89911e65f550e0b76d641fff4703f8ddf1
Cesar <ccordero.007@gmail.cim>
2024-01-08 10:37:52 -0600

    Validate form create event

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useId, Fragment, useRef } from "react"
+import { useState, useMemo, useId, Fragment, useRef, FormEvent } from "react"
 import {
     startOfWeek,
     startOfMonth,
@@ -146,11 +146,64 @@ function EventFormModal({
     ...modalProps }: EventFormModalProps) {
     const isNew = event == null
     const formId = useId()
-    const [selectedColor, setSelectedColor] = useState<string>(event?.color || EVENT_COLORS[0])
+    const [selectedColor, setSelectedColor] = useState(event?.color || EVENT_COLORS[0])
     const [isAllDayChecked, setIsAllDayChecked] = useState(event?.allDay || false)
     const [startTime, setStartTime] = useState(event?.startTime || "")
     const endTimeRef = useRef<HTMLInputElement>(null)
     const nameRef = useRef<HTMLInputElement>(null)
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+
+        const name = nameRef.current?.value
+        const endTime = endTimeRef.current?.value
+
+        // if (
+        //     name == null ||
+        //     name === "" ||
+        //     startTime == null ||
+        //     startTime === "" ||
+        //     endTime == null ||
+        //     endTime === ""
+        // ) {
+        //     return
+        // }
+        if (
+            name == null ||
+            name === ""
+        ) {
+            return
+        }
+
+        const commonProps = {
+            name,
+            date: date || event?.date,
+            color: selectedColor
+        }
+
+        let newEvent: UnionOmit<Event, "id">
+        if (isAllDayChecked) {
+            newEvent = {
+                ...commonProps,
+                allDay: true
+            }
+        } else {
+            if (
+                startTime == null ||
+                startTime === "" ||
+                endTime == null ||
+                endTime === ""
+            ) {
+                return
+            }
+            newEvent = {
+                ...commonProps,
+                allDay: false,
+                startTime,
+                endTime
+            }
+        }
+    }
 
     return (<Modal {...modalProps}>
         <div className="modal-title">
