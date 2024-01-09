f332a57ee5afaa48bf083cd4fff0f83b53efd1f4
Cesar <ccordero.007@gmail.cim>
2024-01-08 21:15:09 -0600

    Show modal when an event is cliked and handle update and delete events

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -151,23 +151,37 @@ function CalendarDay({
 }
 
 function CalendarEvent({ event }: { event: Event }) {
-    return (
-        <button className={cc("event", event.color, event.allDay && "all-day-event")}>
-            {event.allDay ? (
-                <div className="event-name">{event.name}</div>
-            ) : (
-                <>
-                    <div className={`color-dot ${event.color}`}></div>
-                    <div className="event-time">
-                        {formatDate(parse(event.startTime, "HH:mm", event.date), {
-                            timeStyle: "short"
-                        })}
-                    </div>
-                    <div className="event-name">{event.name}</div>
-                </>
-            )}
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+    const { updateEvent, deleteEvent } = useEvents()
 
-        </button>
+    return (
+        <>
+            <button
+                onClick={() => setIsEditModalOpen(true)}
+                className={cc("event", event.color, event.allDay && "all-day-event")}>
+                {event.allDay ? (
+                    <div className="event-name">{event.name}</div>
+                ) : (
+                    <>
+                        <div className={`color-dot ${event.color}`}></div>
+                        <div className="event-time">
+                            {formatDate(parse(event.startTime, "HH:mm", event.date), {
+                                timeStyle: "short"
+                            })}
+                        </div>
+                        <div className="event-name">{event.name}</div>
+                    </>
+                )}
+            </button>
+            <EventFormModal
+                event={event}
+                isOpen={isEditModalOpen}
+                onClose={() => setIsEditModalOpen(false)}
+                onSubmit={e => updateEvent(event.id, e)}
+                onDelete={() => deleteEvent(event.id)}
+            />
+        </>
+
     )
 }
 
